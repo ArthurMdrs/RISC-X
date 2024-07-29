b7bd34c2b43b84a3f057d0e20f20f7933ebab968
valmyr <valmir.silva@ee.ufcg.edu.br>
2024-07-29 19:44:01 -0300

    create module of mult.

---
+++ rtl/Type_M_Instructions/MULT_HANDSHAKE/mult.sv
@@ -0,0 +1,25 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//        CLASS: ls
+//  DESCRIPTION: 
+//         BUGS: ---
+//       AUTHOR: Valmir F. Silva (), valmir.silva@ee.ufcg.edu.br
+// ORGANIZATION: 
+//      VERSION: 1.0
+//      CREATED: 07/29/2024 07:37:29 PM
+//     REVISION: ---
+///////////////////////////////////////////////////////////////////////////////////////////////////
+module mult(
+
+            input logic         clock   ,
+            input logic         nreset  ,
+            input logic         valid   ,
+            input logic [31:0]  a       ,
+            input logic [31:0]  b       ,
+            output logic [31:0] c       ,
+            output logic ready          ,
+
+
+  );
+
+endmodule
