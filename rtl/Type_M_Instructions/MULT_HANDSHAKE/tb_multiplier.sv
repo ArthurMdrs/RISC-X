b7b978e90f1bfe2ea673903640b9efa48dbe7d16
André Medeiros <andre.escariao1@gmail.com>
2024-08-11 00:22:32 -0300

    Atualizando sincronização e valid

--- rtl/Type_M_Instructions/MULT_HANDSHAKE/tb_multiplier.sv
+++ rtl/Type_M_Instructions/MULT_HANDSHAKE/tb_multiplier.sv
@@ -1,8 +1,8 @@
-// ------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------
 // DESCRIÇÃO: Testbench de validação para o RTL mult.sv
 //				- Validação de sinais aleatórios
 //				- Validação de sinais máximos e mínimos
-// ------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------
 // RELEASE HISTORY  :
 // DATA                 VERSÃO      AUTOR  				    DESCRÇÃO
 // 2024-08-10           0.01        André Medeiros     	    Versão inicial.
@@ -11,23 +11,32 @@
 module tb_booth_multiplier;
 
     // Declaração dos sinais
-    reg [31:0] a;
-    reg [31:0] b;
-    reg start;
-    reg ready;         // Adiciona o sinal ready
-    wire [63:0] c;
-    wire valid;        // Adiciona o sinal valid
-    wire done;
+    logic [31:0] a;
+    logic [31:0] b;
+    logic start;
+    logic ready;         // Adiciona o sinal ready
+    logic [63:0] c;
+    logic valid;        // Adiciona o sinal valid
+    logic done;
+
+    logic clk;         // Sinal de clock
+    logic reset_n;     // Sinal de reset ativo baixo
+
+    logic monitor_done;
+
+	always #5 clk = ~clk;  // Clock de 10 ns de período (100 MHz)
 
     // Instancia o módulo a ser testado
-    booth_multiplier_32x32 uut (
+    booth_multiplier_32x32 dut (
         .a(a),
         .b(b),
         .start(start),
         .ready(ready),   // Conecta o sinal ready
         .c(c),
         .valid(valid),   // Conecta o sinal valid
-        .done(done)
+        .done(done),
+        .clk(clk),
+        .reset_n(reset_n)
     );
 
     // Procedimento inicial do testbench
@@ -46,25 +55,26 @@ module tb_booth_multiplier;
         $dumpfile("waves.vcd");
         $dumpvars(0, tb_booth_multiplier);
 
-        $display("-------------------------------------------- INICIALIZANDO -----------------------------------------------------");
+        $display("Simulação iniciada...");
 
         // Inicializa os sinais
+        clk = 0;
         a = 32'b0;
         b = 32'b0;
         start = 0;
         ready = 1; // Inicializa ready como 1 sempre
 
+        reset_n = 0;
+        #100;  // Mantém o reset por um tempo mais longo
+        reset_n = 1;
+
+
         // Espera um pouco e depois aplica o estímulo
         #10;
         $display("Executando testes de valor fixo...");
         a = 32'h0000_0002; // Valor de exemplo para a
-        b = 32'h0000_0006; // Valor de exemplo para b
-        start = 1;      // Inicia a multiplicação
-        #10;          
-        start = 0;
-        wait(valid);
-        wait(done);
-        #50;
+        b = 32'h0000_0004; // Valor de exemplo para b
+        start_test();
 
         // Testes aleatórios
         #10;
@@ -73,69 +83,58 @@ module tb_booth_multiplier;
             // Gera valores aleatórios para a e b
             a = $random;
             b = $random;
-
-            start = 1;
-            #10;          
-            start = 0;
-            wait(valid);
-            wait(done); 
-            #10;
+            start_test();
         end
-        #50;
-
-        // Testes de valores limite
-      	$display("Teste de valores limite...");
 
         // Teste para valores máximos
         $display("Executando teste para valores máximos...");
         #10;
         a = 32'hFFFFFFFF;
         b = 32'hFFFFFFFF;
-        start = 1;
-        #10;
-        start = 0;
-        wait(valid);
-        #50;
+        start_test();
 
         // Teste para um valor pequeno e um valor grande
         $display("Teste para valor pequeno e um grande...");
         a = 32'h00000001;
         b = 32'h7FFFFFFF;
-        start = 1;
-        #10;
-        start = 0;
-        wait(valid);
-        #50;
+        start_test();
 
         // Teste para valores negativos
       	$display("Teste para valores negativos...");
         a = 32'h80000000;
         b = 32'h80000000;
-        start = 1;
-        #10;
-        start = 0;
-        wait(valid);
-        #50;
+        start_test();
         
         // Teste para valores mínimos
         $display("Teste de valores mínimos...");
         a = 32'h00000000;
         b = 32'h00000000;
-        start = 1;
-        //ready = 1;
-        #10;
-        start = 0;
-        //ready = 0;
-        wait(valid);
-        #50;
+        start_test();
 
         // Finaliza a simulação
         $finish;
     end
 
-    // Monitoramento dos sinais
-    initial begin
-        $monitor("Time: %0t | a: %d | b: %d | c: %d | valid: %b | done: %b", $time, a, b, c, valid, done);
+    // Função para iniciar e aguardar o término do teste
+    task start_test();
+        begin
+            start = 1;
+            #10;
+            start = 0;
+            wait(valid);
+            wait(done);
+            #50;
+        end
+    endtask
+
+    // Monitoramento dos sinais quando valid é 1
+    always @(posedge clk) begin
+        if (valid && !monitor_done) begin
+            $display("Time: %0t | a: %d | b: %d | c: %d | valid: %b | done: %b", $time, a, b, c, valid, done);
+            monitor_done = 1;
+        end else if (!valid) begin
+            monitor_done = 0;
+        end
     end
 
 endmodule
