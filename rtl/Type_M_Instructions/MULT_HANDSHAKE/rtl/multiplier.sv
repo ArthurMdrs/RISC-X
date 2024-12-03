95e2acaba74cfe9027b40895451378afde893c85
andreemedeiros <andre.escariao1@gmail.com>
2024-12-03 17:09:58 -0300

    ajustes multiplicador

--- rtl/Type_M_Instructions/MULT_HANDSHAKE/rtl/multiplier.sv
+++ rtl/Type_M_Instructions/MULT_HANDSHAKE/rtl/multiplier.sv
@@ -7,7 +7,7 @@
 // ----------------------------------------------------------------------------------------------------
 // RELEASE HISTORY  :
 // DATA                 VERSÃO      AUTOR     				DESCRIÇÃO
-// 2024-11-27           0.13        André Medeiros     	    Versão inicial.
+// 2024-12-03           0.14        André Medeiros     	    Versão inicial.
 // ------------------------------------------------------------------------------------------------
 
 module multiplier_32x32 (
@@ -23,32 +23,47 @@ module multiplier_32x32 (
     output logic [31:0] resultado          
 );
     // Registradores internos
-    logic signed   [31:0] full_result;
+    logic signed   [63:0] full_result_signed;
+    logic unsigned [63:0] full_result_unsigned;
     logic          valid_reg;
 
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            in_ready_o  <= 1'b1;
-            out_valid_o <= 1'b0;
-            resultado   <= 32'b0;
-            full_result <= 32'b0;
-            valid_reg   <= 1'b0;
+            in_ready_o              <= 1'b1;
+            out_valid_o             <= 1'b0;
+            resultado               <= 32'b0;
+            full_result_signed      <= 64'b0;
+            full_result_unsigned    <= 64'b0;
+            valid_reg               <= 1'b0;
         end else begin
             // Handshake para entrada
             if (in_ready_o && in_valid_i) begin
                 in_ready_o <= 1'b0;
                 case (op_sel)
                     // 4 Operações RISC-V
-                    2'b00: full_result <= ($signed(a) * $signed(b));            
-                    2'b01: full_result <= ($signed(a) * $signed(b)) >>> 32;     
-                    2'b10: full_result <= ($signed(a) * $unsigned(b)) >>> 32;   
-                    2'b11: full_result <= ($unsigned(a) * $unsigned(b)) >>> 32; 
+                    2'b00: full_result_signed <= ($signed(a) * $signed(b));
+                    2'b01: full_result_signed <= ($signed(a) * $signed(b));
+                    2'b10: full_result_signed <= ($signed(a) * $unsigned(b));
+                    2'b11: full_result_unsigned <= ($unsigned(a) * $unsigned(b));
                 endcase
                 valid_reg <= 1'b1;
             end
             // Handshake para saída
             if (valid_reg && out_ready_i) begin
-                resultado <= full_result;
+                case (op_sel)
+                    2'b00: begin
+                        resultado   <= full_result_signed;
+                    end            
+                    2'b01: begin
+                        resultado   <= full_result_signed >>> 32;
+                    end     
+                    2'b10: begin
+                        resultado   <= full_result_signed >>> 32;
+                    end   
+                    2'b11: begin
+                        resultado   <= full_result_unsigned >>> 32;
+                    end
+                endcase
                 out_valid_o <= 1'b1;
                 valid_reg   <= 1'b0;
                 in_ready_o  <= 1'b1;
